619d191bc8f2f699499166e47ae20aa87fd16449
Anderson Barbosa da Costa <andersonb.costa2@gmail.com>
2021-09-10 00:18:57 -0300

    0007 - Ajustes ProductServiceTest

--- desafio/src/test/java/br/com/anderson/costa/desafio/model/service/ProductServiceTest.java
+++ desafio/src/test/java/br/com/anderson/costa/desafio/model/service/ProductServiceTest.java
@@ -17,8 +17,8 @@ import java.util.Optional;
 
 public class ProductServiceTest {
 
-    private final ProductDTO productDtoExpected = new ProductDTO("1", "cadeira", "cadeira gamer pro", 899.56);
-    private final Product productExpected = new Product(1, "cadeira", "cadeira gamer pro", 899.56);
+    private ProductDTO productDtoExpected;
+    private Product productExpected;
 
     @Mock
     private ProductRepository mockRepository;
@@ -28,6 +28,8 @@ public class ProductServiceTest {
     private void beforeEach() {
         MockitoAnnotations.openMocks(this);
         this.productService = new ProductService(mockRepository);
+        this.productDtoExpected = new ProductDTO("1", "cadeira", "cadeira gamer pro", 899.56);
+        this.productExpected = new Product(1, "cadeira", "cadeira gamer pro", 899.56);
     }
 
     @Test
@@ -45,7 +47,7 @@ public class ProductServiceTest {
 
     @Test
     public void mustReturnListProductsWithMinorPrice() {
-        Mockito.when(mockRepository.findByPriceLessThanEqual(Double.parseDouble("899.56")))
+        Mockito.when(mockRepository.findByPriceLessThanEqual(Mockito.anyDouble()))
                 .thenReturn(Collections.singletonList(products().get(0)));
         var listProductsDTO = productService.getProductsPriceLessThan("899.56");
         Assertions.assertEquals(899.56, listProductsDTO.get(0).getPrice());
@@ -53,7 +55,7 @@ public class ProductServiceTest {
 
     @Test
     public void mustReturnListProductsWithPriceGreaterOrEqualsThan() {
-        Mockito.when(mockRepository.findByPriceGreaterThanEqual(Double.parseDouble("899.56")))
+        Mockito.when(mockRepository.findByPriceGreaterThanEqual(Mockito.anyDouble()))
                 .thenReturn(products());
         var listProductsDTO = productService.getProductsPriceGreaterThan("899.56");
         Assertions.assertEquals(productsDto(), listProductsDTO);
@@ -61,7 +63,7 @@ public class ProductServiceTest {
 
     @Test
     public void mustReturnListProductsWithPriceMinorOrEqualsThanAndGreaterOrEqualsThan() {
-        Mockito.when(mockRepository.findByPriceBetween(Double.parseDouble("1"), Double.parseDouble("999999.99")))
+        Mockito.when(mockRepository.findByPriceBetween(Mockito.anyDouble(), Mockito.anyDouble()))
                 .thenReturn(products());
         var listProductsDTO = productService.getProductsByMinAndGreaterPrice("1", "999999.99");
         Assertions.assertEquals(productsDto(), listProductsDTO);
@@ -69,7 +71,7 @@ public class ProductServiceTest {
 
     @Test
     public void mustNotReturnListProductsWithContainNameOrDescriptionEqual() {
-        Mockito.when(mockRepository.findByNameContains("cadeira"))
+        Mockito.when(mockRepository.findByNameContains(Mockito.anyString()))
                 .thenReturn(Collections.singletonList(products().get(0)));
         var listProductsDTO = productService.getProductsByNameOrDescription("cadeira");
         Assertions.assertNotEquals(productsDto().get(0).getDescription(), listProductsDTO.get(0).getName());
@@ -77,42 +79,42 @@ public class ProductServiceTest {
 
     @Test
     public void mustReturnProductWhenIdValidIsPassed() {
-        Mockito.when(mockRepository.findById(1)).thenReturn(Optional.of(productExpected));
+        Mockito.when(mockRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(productExpected));
         final Optional<Product> productById = productService.getProductById("1");
         Assertions.assertEquals(productExpected, productById.get());
     }
 
     @Test
     public void mustNotReturnProductWhenIdInValidIsPassed() {
-        Mockito.when(mockRepository.findById(2)).thenReturn(Optional.empty());
+        Mockito.when(mockRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
         final Optional<Product> productById = productService.getProductById("2");
         Assertions.assertFalse(productById.isPresent());
     }
 
     @Test
     public void mustReturnListProductsWithFilterMinPrice() {
-        Mockito.when(mockRepository.findByPriceLessThanEqual(10000.0)).thenReturn(products());
+        Mockito.when(mockRepository.findByPriceLessThanEqual(Mockito.anyDouble())).thenReturn(products());
         final List<ProductDTO> responseFilter = productService.getResponseFilter("10000.0", null, null);
         Assertions.assertEquals(2, responseFilter.size());
     }
 
     @Test
     public void mustReturnListProductsWithFilterMaxPrice() {
-        Mockito.when(mockRepository.findByPriceGreaterThanEqual(3899.56)).thenReturn(products());
+        Mockito.when(mockRepository.findByPriceGreaterThanEqual(Mockito.anyDouble())).thenReturn(products());
         final List<ProductDTO> responseFilter = productService.getResponseFilter(null, "3899.56", null);
         Assertions.assertEquals(2, responseFilter.size());
     }
 
     @Test
     public void mustReturnListProductsWithNameOrDescription() {
-        Mockito.when(mockRepository.findByNameContains("cadeira")).thenReturn(Collections.singletonList(products().get(0)));
+        Mockito.when(mockRepository.findByNameContains(Mockito.anyString())).thenReturn(Collections.singletonList(products().get(0)));
         final List<ProductDTO> responseFilter = productService.getResponseFilter(null, null, "cadeira");
         Assertions.assertEquals(1, responseFilter.size());
     }
 
     @Test
     public void mustReturnListProductsWithMinorAndGreaterAndNameOrDescription() {
-        Mockito.when(mockRepository.findPriceBetweenNameEquals(1.0, 3899.56, "cadeira")).thenReturn(products());
+        Mockito.when(mockRepository.findPriceBetweenNameEquals(Mockito.anyDouble(), Mockito.anyDouble(), Mockito.anyString())).thenReturn(products());
         final List<ProductDTO> responseFilter = productService.getResponseFilter("1.0", "3899.56", "cadeira");
         Assertions.assertEquals(2, responseFilter.size());
     }
@@ -126,7 +128,7 @@ public class ProductServiceTest {
 
     @Test
     public void mustReturnListProductsWithFilterBetweenMinAndMaxPrice() {
-        Mockito.when(mockRepository.findByPriceBetween(1, 10000)).thenReturn(products());
+        Mockito.when(mockRepository.findByPriceBetween(Mockito.anyDouble(), Mockito.anyDouble())).thenReturn(products());
         final List<ProductDTO> responseFilter = productService.getResponseFilter("1", "10000", null);
         Assertions.assertEquals(2, responseFilter.size());
     }
@@ -148,7 +150,7 @@ public class ProductServiceTest {
     public void mustReturnProductWhenValuesAndIdAreValid() {
 
         Mockito.when(mockRepository.save(Mockito.any(Product.class))).thenReturn(productExpected);
-        Mockito.when(mockRepository.findById(Mockito.any(Integer.class))).thenReturn(Optional.of(productExpected));
+        Mockito.when(mockRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(productExpected));
         final ProductDTO productDTO = productService.createOrUpdateProduct(productDtoExpected, "1");
         Assertions.assertEquals(productDtoExpected, productDTO);
         Mockito.verify(mockRepository, Mockito.times(1)).findById(1);
@@ -156,7 +158,7 @@ public class ProductServiceTest {
 
     @Test
     public void mustReturnFalseWhenValuesAndIdNotAreValid() {
-        Mockito.when(mockRepository.findById(Mockito.any(Integer.class))).thenReturn(Optional.empty());
+        Mockito.when(mockRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
         final ProductDTO productDTO = productService.createOrUpdateProduct(productDtoExpected, "3");
         Assertions.assertNull(productDTO);
         Mockito.verify(mockRepository, Mockito.times(1)).findById(3);
@@ -165,19 +167,17 @@ public class ProductServiceTest {
     @Test
     public void mustDeleteProductWhenIdIsValid() {
 
-        Mockito.when(mockRepository.findById(Mockito.any(Integer.class))).thenReturn(Optional.of(productExpected));
+        Mockito.when(mockRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(productExpected));
         final boolean isRemoved = productService.removeProduct(String.valueOf(productExpected.getId()));
         Assertions.assertTrue(isRemoved);
         Mockito.verify(mockRepository, Mockito.times(1)).findById(1);
         Mockito.verify(mockRepository, Mockito.times(1)).delete(productExpected);
     }
 
-
-
     @Test
     public void mustNotDeleteProductWhenIdInValid() {
 
-        Mockito.when(mockRepository.findById(Mockito.any(Integer.class))).thenReturn(Optional.empty());
+        Mockito.when(mockRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
         final boolean isRemoved = productService.removeProduct("3");
         Assertions.assertFalse(isRemoved);
         Mockito.verify(mockRepository, Mockito.times(1)).findById(3);

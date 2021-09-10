c6e4bde626bedf7a53de9ade2b5902ab83e75f16
Anderson Barbosa da Costa <andersonb.costa2@gmail.com>
2021-09-10 00:04:24 -0300

    0006 - Autenticação com JWT e Spring Security

--- desafio/src/main/java/br/com/anderson/costa/desafio/config/SecurityConfig.java
+++ desafio/src/main/java/br/com/anderson/costa/desafio/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package br.com.anderson.costa.desafio.config;
 
+import br.com.anderson.costa.desafio.jwt.filter.JWTAuthenticationFilter;
+import br.com.anderson.costa.desafio.jwt.filter.JWTLoginFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -36,9 +41,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+             http
+                .csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
+                .and().addFilterBefore(new JWTLoginFilter("/auth", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .httpBasic();
     }
---
+++ desafio/src/main/java/br/com/anderson/costa/desafio/jwt/UserCredentials.java
@@ -0,0 +1,10 @@
+package br.com.anderson.costa.desafio.jwt;
+
+import lombok.Data;
+
+@Data
+public class UserCredentials {
+
+    private String username;
+    private String password;
+}
